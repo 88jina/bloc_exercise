5b4f30d65e4c8a95bf5a0ca8060948e6e93a6539
jinapark <jina940323@gmail.com>
2022-02-24 14:49:05 +0900

    counter app

---
+++ lib/app.dart
@@ -0,0 +1,7 @@
+import 'package:bloc_exercise/view/counter_page.dart';
+import 'package:flutter/material.dart';
+
+class CounterApp extends MaterialApp{
+  const CounterApp({Key? key}) : super(key: key, home: const CounterPage());
+
+}
